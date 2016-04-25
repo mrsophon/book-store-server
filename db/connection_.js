09b68e80628be6ff121db930701a94a25f7edb87
Sophon T <mrsophon@gmail.com>
2016-04-25 13:34:01 +0700

    update

--- db/connection_.js
+++ db/connection_.js
@@ -1,7 +1,6 @@
 var mongojs = require('mongojs');
 //var db = mongojs('mongodb://admin:admin123@ds031962.mongolab.com:31962/ionicbookstoreapp', ['users', 'books']);
-var db = mongojs('mongodb://admin:admin123@ds019481.mlab.com:19481/ionicbookstoreapp', ['users', 'books']);
-//var db = mongojs('ionicbookstoreapp', ['users', 'books']);
+var db = mongojs('ionicbookstoreapp', ['users', 'books']);
 
 // Add a unique index
 db.users.ensureIndex({
